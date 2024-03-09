c71f45c408d01ccd6c41d850bad936e0fabfc37d
Masanaser <masadawood29@gmail.com>
2024-03-09 23:54:10 +0200

    part2

--- src/Components/notFound/NotFound.jsx
+++ src/Components/notFound/NotFound.jsx
@@ -4,18 +4,17 @@ import { Link } from 'react-router-dom'
 export default function NotFound() {
   return (
     <>
-<div className="mt-5">
-  <h1 className="notFoundTitle">This page doesn’t exist.</h1>
-  <section className="error-container">
-    <span className="four"><span className="screen-reader-text">4</span></span>
-    <span className="zero"><span className="screen-reader-text">0</span></span>
-    <span className="four"><span className="screen-reader-text">4</span></span>
-  </section>
-  <div className="link-container">
-    <Link to="/" className="btn btn-outline-dark btn-lg">Back Home</Link>
-  </div>
-</div>
-
-
+      <div className="mt-5">
+        <h1 className="notFoundTitle">This page doesn’t exist.</h1>
+        <section className="error-container">
+          <span className="four"><span className="screen-reader-text">4</span></span>
+          <span className="zero"><span className="screen-reader-text">0</span></span>
+          <span className="four"><span className="screen-reader-text">4</span></span>
+        </section>
+        <div className="link-container">
+          <Link to="/" className="btn btn-outline-dark btn-lg">Back Home</Link>
+        </div>
+      </div>
     </>
-  )}
+  )
+}
